f03954c2d720de0f5e5b8ceea636a7805a321608
suminkim <tnals1564@naver.com>
2023-06-27 14:56:39 +0900

    100개의 스레드로 재고감소 요청 테스트 완료 - Race Condition 발생확인

--- src/test/java/com/smsoft/stock/service/StockServiceTest.java
+++ src/test/java/com/smsoft/stock/service/StockServiceTest.java
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -39,5 +44,32 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100번_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i=0; i < threadCount; i++){
+            executorService.submit(()->{
+                try{
+                    stockService.decrease(1L, 1L);
+                }finally {
+                    latch.countDown();
+                }
+
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // stcok 100 -> -(1*100) -> 0
+        assertEquals(0L, stock.getQuantity());
+
     }
 }
