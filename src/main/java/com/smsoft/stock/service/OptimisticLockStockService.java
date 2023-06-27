804f1be220d25191147637132ae8cd7de9ce83b1
suminkim <tnals1564@naver.com>
2023-06-27 21:31:22 +0900

    2. mysql의 Optimistic Lock 사용해서 해결 (version)

---
+++ src/main/java/com/smsoft/stock/service/OptimisticLockStockService.java
@@ -0,0 +1,22 @@
+package com.smsoft.stock.service;
+
+import com.smsoft.stock.domain.Stock;
+import com.smsoft.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class OptimisticLockStockService {
+    private StockRepository stockRepository;
+
+    public OptimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+    @Transactional
+    public void decrease(Long id, Long quantity){
+        Stock stock = stockRepository.findByWithOptimisticLock(id);
+        stock.decrease(quantity);
+        stockRepository.saveAndFlush(stock);
+    }
+}
